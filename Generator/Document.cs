bcd36b6d6768817717ab8eae29cabef5bc3b44ba
Roberto Fagundes de Campos <roberto.fagundes@cidtech.com.br>
2018-08-09 10:06:34 -0300

    Adição da classe Document ao projeto Generator

---
+++ Generator/Document.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Printing;
+
+namespace Generator
+{
+    public class Document
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int MarginLeft { get; set; }
+        public int MarginTop { get; set; }
+        public int MarginRight { get; set; }
+        public int MarginBottom { get; set; }
+    }
+}
